1f72b9102431ec89932f82ce7ea8f30033b3beef
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-14 05:13:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Statement__days_of_the_week_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter a day between 1 and 6");
+            int number = int.Parse(Console.ReadLine()); //convert to an int
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wenesday");
+                    break;
+                case 4: 
+                    Console.WriteLine("Thurday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+
+
+
+            }
+
+            if(number >= 6)
+            {
+                Console.WriteLine("Its a weekend");
+
+            }
+
+            Console.Read();
+        }
+    }
+}
